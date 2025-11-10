cb10030f8f164ca52fe6775a2c684f2e3b307f17
vhthai-max <thaiviethuy1112@gmail.com>
2025-11-10 18:58:04 +1100

    Create index.js

---
+++ 1.3P/index.js
@@ -0,0 +1,7 @@
+// SIT725 Task 1.3P - Introduction to Programming
+function greet(name) {
+    return `Hello, ${name}! Welcome to SIT725.`;
+}
+
+let studentName = "Thai"; 
+console.log(greet(studentName));
